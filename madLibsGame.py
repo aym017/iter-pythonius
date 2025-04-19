1efa0b75825da638e0195c0e4b50e117c0333f9d
AymanA <aymanabas017@gmail.com>
2025-04-19 15:45:35 +0300

    Initial commit: made a game

---
+++ madLibsGame.py
@@ -0,0 +1,11 @@
+place = input("Enter a random place: ")
+cret = input("Enter a random creature: ")
+obj = input("Enter a random object (plural): ")
+building = input("Enter a random building: ")
+person = input("Enter a random person: ")
+
+print("\nThis morning in " + place + ",")
+print("A man-eating " + cret + " was spotted causing chaos.")
+print("It was chasing pedestrians with " + obj + "." )
+print("It burned down the " + building + " to everyone's surprise")
+print("It ran away once it saw " + person + " , who saved the day!")
